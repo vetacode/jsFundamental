9c8f157caba776c506e5ffa6e7439317f1031c20
vetacode <vetacode@gmail.com>
2025-10-30 18:20:28 +0700

    solving prettierrc and settings.json alignment files

--- incredecrement.js
+++ incredecrement.js
@@ -1,11 +1,17 @@
 let a = 4;
 a++;
-console.log(a)
+console.log(a);
 ++a;
-console.log(a)
+console.log(a);
 
 let b = 6;
 console.log('second b:', --b);
 console.log('first b:', b--);
 
 //The prefix form returns the new value while the postfix form returns the old value (prior to increment/decrement).
+
+export default function () {
+  let x = 10;
+
+  return console.log(x);
+}
