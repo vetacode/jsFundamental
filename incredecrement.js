a7b799f9ad3fbe12a3e9458db320322ce6895a84
vetacode <vetacode@gmail.com>
2025-10-30 19:52:30 +0700

    adding postfix decrement

--- incredecrement.js
+++ incredecrement.js
@@ -5,8 +5,9 @@ console.log(a);
 console.log(a);
 
 let b = 6;
-console.log('second b:', --b);
-console.log('first b:', b--);
+console.log('second b:', --b); //5
+console.log('first b:', b--); //5
+console.log('third b:', --b); //3
 
 //The prefix form returns the new value while the postfix form returns the old value (prior to increment/decrement).
 
