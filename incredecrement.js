93c84a84c23e5fd650b5095c54f2421eed62cc0b
vetacode <vetacode@gmail.com>
2025-10-30 17:57:41 +0700

    update logic on incredecrement

--- incredecrement.js
+++ incredecrement.js
@@ -1,6 +1,11 @@
 let a = 4;
 a++;
 console.log(a)
+++a;
+console.log(a)
 
 let b = 6;
-console.log(b--);
+console.log('second b:', --b);
+console.log('first b:', b--);
+
+//The prefix form returns the new value while the postfix form returns the old value (prior to increment/decrement).
