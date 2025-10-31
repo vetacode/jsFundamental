ede4540b9a0080a1e6494c53f2aa9e45cf3bcae4
vetacode <vetacode@gmail.com>
2025-10-31 12:55:52 +0700

    adding logicalAND

---
+++ logicalAND.js
@@ -0,0 +1,6 @@
+let hour = 12;
+let minute = 30;
+
+if (hour == 12 && minute == 30) {
+  console.log('The time is 12:30'); //both conditiion should be True, then return True and the conditiion runs
+}
