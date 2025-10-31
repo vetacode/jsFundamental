13a677752a249378b3ffe102ef1eee86d1521895
vetacode <vetacode@gmail.com>
2025-10-31 12:48:40 +0700

    adding logicalOR operator

---
+++ logicalOR.js
@@ -0,0 +1,4 @@
+let firstName = '';
+let lastName = '';
+let nickName = 'SuperCoder';
+console.log(firstName || lastName || nickName || 'Anonymous'); //Logical OR will return the original value of the variable
