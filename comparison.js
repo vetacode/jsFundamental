0b220dc690fba6c007d37e5bd5f947f8865a6e75
vetacode <vetacode@gmail.com>
2025-10-31 11:12:36 +0700

    adding undefined comparison

--- comparison.js
+++ comparison.js
@@ -11,3 +11,7 @@ console.log(null >= 0); // (3) true
 //equality check == and comparisons > < >= <= work differently. Comparisons convert null to a number, treating it as 0. That’s why (3) null >= 0 is true and (1) null > 0 is false.
 
 //On the other hand, the equality check == for undefined and null is defined such that, without any conversions, they equal each other and don’t equal anything else. That’s why (2) null == 0 is false.
+
+console.log('undefined 1:', undefined > 0); // false (1)
+console.log('undefined 2:', undefined < 0); // false (2)
+console.log('undefined 3:', undefined == 0); // false (3)
