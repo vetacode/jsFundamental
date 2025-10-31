c15b613eef75b8f9f351ec44fc10ffd1419d9706
vetacode <vetacode@gmail.com>
2025-10-31 13:53:13 +0700

    adding more logicalOpAssignment model

--- logicalOpAssignment.js
+++ logicalOpAssignment.js
@@ -2,8 +2,9 @@ console.log(console.log(1) || 2 || console.log(3));
 //console.log(1) return 1, then it return undefined when outer console.log calling it return the first next truthy value which is 2, then conosle log 2.
 //the 3rd console.log never runs coz the outer console.log has got the returned truthy value (2)
 
-let age = 30;
-
-if (age == 14 && age == 90) {
+let age = 14;
+if (age >= 14 && age <= 90) {
   console.log('you are in!');
+} else {
+  console.log('Sorry, maybe in the next life chapter');
 }
