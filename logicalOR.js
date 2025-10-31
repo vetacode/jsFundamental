146491e876599e6f10855290c89765d267dec2d6
vetacode <vetacode@gmail.com>
2025-10-31 12:52:33 +0700

    adding short circuited feature of OR

--- logicalOR.js
+++ logicalOR.js
@@ -2,3 +2,8 @@ let firstName = '';
 let lastName = '';
 let nickName = 'SuperCoder';
 console.log(firstName || lastName || nickName || 'Anonymous'); //Logical OR will return the original value of the variable
+
+//Short-circuit evaluation feature
+
+true || console.log('not printed'); //it will not print coz the truthy value has been returned (short-circuited) in the first evaluation
+false || console.log('printed'); //only this result printed
