fe22c7e2d879f08b4ec9f85ff668c984ec691eee
vetacode <vetacode@gmail.com>
2025-11-01 11:57:22 +0700

    adding forLoop

---
+++ forLoop.js
@@ -0,0 +1,35 @@
+//Basic syntax of for loop:
+// for (begin; condition; step) {
+//   // ... loop body ...
+// }
+
+for (let i = 0; i < 3; i++) {
+  console.log(i);
+}
+
+//the algorithm steps:
+// Run begin
+// → (if condition → run body and run step) //0
+// → (if condition → run body and run step) //1
+// → (if condition → run body and run step) //2
+// → false condition (i == 3) -> stop iterating
+
+//Bentuk lain supaya variable bisa diakses diluar
+let i = 0;
+for (i = 0; i < 3; i++) {
+  // use an existing variable
+  console.log(i); // 0, 1, 2
+}
+console.log(i); // 3, visible, because declared outside of the loop
+
+//bentuk lebih simplenya:
+let p = 0;
+for (; p < 3; p++) console.log('nilai p:', p);
+
+//juga bisa remove bagian step:
+let q = 0;
+for (; q < 3; ) console.log('bilai q:', q++);
+
+//bisa juga hilangkan semua bagian (begin;condition;step)
+let z = 0;
+for (;;) console.log('nilai z;', z); //akan membuat infinite loops
