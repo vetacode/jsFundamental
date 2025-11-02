6cbf95c90b5f364ee9c2353fe139eeeb1a49aa17
vetacode <vetacode@gmail.com>
2025-11-02 21:29:03 +0700

    adding switch

---
+++ switch.js
@@ -0,0 +1,11 @@
+let day = 'Sunday';
+switch (day) {
+  case 'Sunday':
+    console.log('Yeay it is  Holiday');
+    break;
+  case 'Monday':
+    console.log('Work Work Work!');
+    break;
+  default:
+    console.log(`You have to visit calendar NOW!`);
+}
