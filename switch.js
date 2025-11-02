75e3f2a49abed338cda1b4c36fdcdadfb7f1076f
vetacode <vetacode@gmail.com>
2025-11-02 21:56:37 +0700

    converting ifElse to switch

--- switch.js
+++ switch.js
@@ -56,3 +56,30 @@ if (browser === 'Edge') {
 ) {
   console.log('Okay we support these browsers too');
 } else console.log('We hope that this page looks ok!');
+
+//convert to switch!
+let a = 1;
+
+// if (a == 0) {
+//   alert(0);
+// }
+// if (a == 1) {
+//   alert(1);
+// }
+
+// if (a == 2 || a == 3) {
+//   alert('2,3');
+// }
+
+switch (2) {
+  case 0:
+    console.log(0);
+    break;
+  case 1:
+    console.log(1);
+    break;
+  case 2:
+  case 3:
+    console.log('2 or 3');
+    break;
+}
