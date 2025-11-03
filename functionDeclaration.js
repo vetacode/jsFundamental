407fec4787a688777e62889b1a966a4f70d56ffd
vetacode <vetacode@gmail.com>
2025-11-03 09:56:52 +0700

    add remarks on functionDeclaration

--- functionDeclaration.js
+++ functionDeclaration.js
@@ -1,7 +1,13 @@
-function getNumber() {
-  let angka = 5;
-  console.log(angka);
+let nama = 'Mosley'; //variable luar (global) hanya digunakan apabila tidak ada variable di local
+
+function cobaFungsi() {
+  let umur = 45;
+  console.log('Nama:', nama); //variable luar (global) dapat dikses dari dalam/local function
+  console.log('Umur:', umur);
+
+  nama = 'Bob';
+  console.log('Ganti Nama:', nama); //variable luar hanya digunakan apabila tidak ada variable di local
 }
 
 // console.log(angka); //error, variable hanya dapat diakses local di dalam scope function
-getNumber();
+cobaFungsi();
