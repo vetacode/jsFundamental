6fae6c754accea4389f1d5c87284539b3ee2282f
vetacode <vetacode@gmail.com>
2025-11-04 15:27:14 +0700

    good vs bad comments added

--- codeRefactor.js
+++ codeRefactor.js
@@ -15,7 +15,7 @@ for (let t = 0; t < 3; t++) {
 
 // ...
 
-//The code should easy to read and indicate the meaning. Refactored code:
+//The code should easy to read and dunction should tell themeselves what's going on. Refactored code:
 addWhiskey(glass);
 addJuice(glass);
 
@@ -32,3 +32,26 @@ function addJuice(container) {
     //...
   }
 }
+
+//Good comments:
+/*
+ eturns x raised to the n-th power.
+
+ @param {number} x The number to raise.
+ @param {number} n The power, must be a natural number.
+  @return {number} x raised to the n-th power.
+ */
+function pow(x, n) {
+  // ...
+}
+
+/*
+Comment this:
+Overall architecture, high-level view.
+Function usage.
+Important solutions, especially when not immediately obvious.
+
+Avoid comments:
+That tell “how code works” and “what it does”.
+Put them in only if it’s impossible to make the code so simple and self-descriptive that it doesn’t require them.
+*/
